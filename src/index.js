67773f72dc5b3a60fce2f24f857003fad5f5726d
Roman <mineyev2@illinois.edu>
2025-08-05 00:12:28 -0400

    trying to add particles

--- src/index.js
+++ src/index.js
@@ -3,13 +3,61 @@ import "./scss/main.ltr.scss";
 import "./scss/themes.scss";
 import "./scss/blue.scss";
 import "./scss/custom.scss";
-
+import { tsParticles } from "tsparticles-slim";
 import imagesLoaded from "imagesloaded";
-
 import SiteState from './js/SiteState';
 import Site from './js/Site';
 
+// // Single initialization point
+// document.addEventListener("DOMContentLoaded", () => {
+//   // Initialize your site
+//   imagesLoaded('.preloadimage', function() {    
+//     const siteState = new SiteState();
+//     new Site(siteState);
+//   });
+
+//   // Initialize particles
+//   loadFull().then((engine) => {
+//     engine.load("background", particlesConfig)
+//       .then(() => {
+//         console.log("Particles loaded successfully");
+//       })
+//       .catch((error) => {
+//         console.error("Error loading particles:", error);
+//       });
+//   });
+// });
+
+// setTimeout(() => {
+//     const canvas = document.querySelector('#background canvas');
+//     console.log('Canvas element:', canvas);
+//     if (canvas) {
+//       console.log('Canvas dimensions:', canvas.width, 'x', canvas.height);
+//     }
+//   }, 1000);
+
 imagesLoaded('.preloadimage', function() {    
-    const siteState = new SiteState();
-    new Site(siteState);
+const siteState = new SiteState();
+new Site(siteState);
+});
+
+// Initialize particles
+tsParticles.load("background", {
+    particles: {
+        number: { value: 50 },
+        color: { value: "#ffffff" },
+        shape: { type: "circle" },
+        opacity: { value: 0.5 },
+        size: { value: 3 },
+        move: {
+            enable: true,
+            speed: 2
+        }
+    },
+    interactivity: {
+        events: {
+            onHover: { enable: true, mode: "repulse" },
+            onClick: { enable: true, mode: "push" }
+        }
+    }
 });
